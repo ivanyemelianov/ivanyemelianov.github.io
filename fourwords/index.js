9f4037016b097adff446087a066e320ef1fa1715
Evan <ivan.emelyanov.94@gmail.com>
2023-06-20 20:46:54 -0700

    Updated dictionary

--- fourwords/index.js
+++ fourwords/index.js
@@ -58,7 +58,7 @@ function loseWinCheck() {
           window.location.reload();
         }
     }
-    if (wordCount >= 10)
+    if (wordCount === 21)
     {
         const result = window.confirm("You won");
       
