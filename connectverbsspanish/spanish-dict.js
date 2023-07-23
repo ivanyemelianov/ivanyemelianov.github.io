b83b95bfd662f873e438678dedfff5c8d1ec1724
Evan <ivan.emelyanov.94@gmail.com>
2023-07-23 00:50:05 -0700

    Lib Update 1.1

--- connectverbsspanish/spanish-dict.js
+++ connectverbsspanish/spanish-dict.js
@@ -1,16 +1,80 @@
 const dict = new Map([
-  ['speak (hablar)', 'hablo hablas habla hablamos habláis hablan'],
-  ['eat (comer)', 'como comes come comemos coméis comen'],
-  ['live (vivir)', 'vivo vives vive vivimos vivís viven'],
-  ['be (ser)', 'soy eres es somos sois son'],
-  ['be (estar)', 'estoy estás está estamos estáis están'],
-  ['have (tener)', 'tengo tienes tiene tenemos tenéis tienen'],
-  ['go (ir)', 'voy vas va vamos vais van'],
-  ['do/make (hacer)', 'hago haces hace hacemos hacéis hacen'],
-  ['see (ver)', 'veo ves ve vemos veis ven'],
-  ['know (saber)', 'sé sabes sabe sabemos sabéis saben'],
-  ['say (decir)', 'digo dices dice decimos decís dicen'],
-  ['can (poder)', 'puedo puedes puede podemos podéis pueden'],
-  ['give (dar)', 'doy das da damos dais dan'],
-  ['come (venir)', 'vengo vienes viene venimos venís vienen'],
+  //LLevar
+  ["take (llevar, Present)", "llevo llevas lleva llevamos lleváis llevan"],
+  ["take (llevar, Preterite)", "llevé llevaste llevó llevamos llevasteis llevaron"],
+  ["take (llevar, Imperfect)", "llevaba llevabas llevaba llevábamos llevabais llevaban"],
+  ["take (llevar, Future)", "llevaré llevarás llevará llevaremos llevaréis llevarán"],
+  ["take (llevar, Conditional)", "llevaría llevarías llevaría llevaríamos llevaríais llevarían"],
+
+  //Ser
+  ["be (ser, Present)", "soy eres es somos sois son"],
+  ["be (ser, Preterite)", "fui fuiste fue fuimos fuisteis fueron"],
+  ["be (ser, Imperfect)", "era eras era éramos erais eran"],
+  ["be (ser, Future)", "seré serás será seremos seréis serán"],
+  ["be (ser, Conditional)", "sería serías sería seríamos seríais serían"],
+
+  //Torcar
+  ["twist (torcer, Present)", "tuerzo tuerces tuerce torcemos torcéis tuercen"],
+  ["twist (torcer, Preterite)", "torcí torciste torció torcimos torcisteis torcieron"],
+  ["twist (torcer, Imperfect)", "torcía torcías torcía torcíamos torcíais torcían"],
+  ["twist (torcer, Future)", "torceré torcerás torcerá torceremos torceréis torcerán"],
+  ["twist (torcer, Conditional)", "torcería torcerías torcería torceríamos torceríais torcerían"],
+
+  //Estar
+  ["be (estar, Present)", "estoy estás está estamos estáis están"],
+  ["be (estar, Preterite)", "estuve estuviste estuvo estuvimos estuvisteis estuvieron"],
+  ["be (estar, Imperfect)", "estaba estabas estaba estábamos estabais estaban"],
+  ["be (estar, Future)", "estaré estarás estará estaremos estaréis estarán"],
+  ["be (estar, Conditional)", "estaría estarías estaría estaríamos estaríais estarían"],
+
+  //Reprochar
+  ["reproach (reprochar, Present)", "reprocho reprochas reprocha reprochamos reprocháis reprochan"],
+  ["reproach (reprochar, Preterite)", "reproché reprochaste reprochó reprochamos reprochasteis reprocharon"],
+  ["reproach (reprochar, Imperfect)", "reprochaba reprochabas reprochaba reprochábamos reprochabais reprochaban"],
+  ["reproach (reprochar, Future)", "reprocharé reprocharás reprochará reprocharemos reprocharéis reprocharán"],
+  ["reproach (reprochar, Conditional)", "reprocharía reprocharías reprocharía reprocharíamos reprocharíais reprocharían"],
+
+  ["neglect (Present)", "descuido descuidas descuida descuidamos descuidáis descuidan"],
+  ["neglect (Preterite)", "descuidé descuidaste descuidó descuidamos descuidasteis descuidaron"],
+  ["neglect (Imperfect)", "descuidaba descuidabas descuidaba descuidábamos descuidabais descuidaban"],
+  ["neglect (Future)", "descuidaré descuidarás descuidará descuidaremos descuidaréis descuidarán"],
+  ["neglect (Conditional)", "descuidaría descuidarías descuidaría descuidaríamos descuidaríais descuidarían"],
+
+  ["go out (Present)", "salgo sales sale salimos salís salen"],
+  ["go out (Preterite)", "salí saliste salió salimos salisteis salieron"],
+  ["go out (Imperfect)", "salía salías salía salíamos salíais salían"],
+  ["go out (Future)", "saldré saldrás saldrá saldremos saldréis saldrán"],
+  ["go out (Conditional)", "saldría saldrías saldría saldríamos saldríais saldrían"],
+
+  ["put (Present)", "pongo pones pone ponemos ponéis ponen"],
+  ["put (Preterite)", "puse pusiste puso pusimos pusisteis pusieron"],
+  ["put (Imperfect)", "ponía ponías ponía poníamos poníais ponían"],
+  ["put (Future)", "pondré pondrás pondrá pondremos pondréis pondrán"],
+  ["put (Conditional)", "pondría pondrías pondría pondríamos pondríais pondrían"],
+
+  ["exchange (Present)", "intercambio intercambias intercambia intercambiamos intercambiáis intercambian"],
+  ["exchange (Preterite)", "intercambié intercambiaste intercambió intercambiamos intercambiasteis intercambiaron"],
+  ["exchange (Imperfect)", "intercambiaba intercambiabas intercambiaba intercambiábamos intercambiabais intercambiaban"],
+  ["exchange (Future)", "intercambiaré intercambiarás intercambiará intercambiaremos intercambiaréis intercambiarán"],
+  ["exchange (Conditional)", "intercambiaría intercambiarías intercambiaría intercambiaríamos intercambiaríais intercambiarían"],
+
+  ["answer (Present)", "contesto contestas contesta contestamos contestáis contestan"],
+  ["answer (Preterite)", "contesté contestaste contestó contestamos contestasteis contestaron"],
+  ["answer (Imperfect)", "contestaba contestabas contestaba contestábamos contestabais contestaban"],
+  ["answer (Future)", "contestaré contestarás contestará contestaremos contestaréis contestarán"],
+  ["answer (Conditional)", "contestaría contestarías contestaría contestaríamos contestaríais contestarían"],
+
+  ["disappear (Present)", "desaparezco desapareces desaparece desaparecemos desaparecéis desaparecen"],
+  ["disappear (Preterite)", "desaparecí desapareciste desapareció desaparecimos desaparecisteis desaparecieron"],
+  ["disappear (Imperfect)", "desaparecía desaparecías desaparecía desaparecíamos desaparecíais desaparecían"],
+  ["disappear (Future)", "desapareceré desaparecerás desaparecerá desapareceremos desapareceréis desaparecerán"],
+  ["disappear (Conditional)", "desaparecería desaparecerías desaparecería desapareceríamos desapareceríais desaparecerían"],
+
+  ["thank (Present)", "agradezco agradeces agradece agradecemos agradecéis agradecen"],
+  ["thank (Preterite)", "agradecí agradeciste agradeció agradecimos agradecisteis agradecieron"],
+  ["thank (Imperfect)", "agradecía agradecías agradecía agradecíamos agradecíais agradecían"],
+  ["thank (Future)", "agradeceré agradecerás agradecerá agradeceremos agradeceréis agradecerán"],
+  ["thank (Conditional)", "agradecería agradecerías agradecería agradeceríamos agradeceríais agradecerían"],
+
+
 ]);
